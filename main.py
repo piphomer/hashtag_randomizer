60da938ccc522511150b1fdfac636a7865beadee
piphomer <phillippeterhomer@gmail.com>
2023-04-18 22:28:46 +0100

    WIP #2

--- main.py
+++ main.py
@@ -3,23 +3,23 @@
 import random
 import csv
 
-def read_txt():
-    f = open("F:/Michelle/hashtag_file.txt", "r") #open text file
-
-    hashtag_list = f.readlines()
-
-    hashtag_list = list(map(str.strip, hashtag_list)) #get rid of the \r tag from each line
-
-    f.close()
-
-    return hashtag_list
+# def read_txt():
+#     f = open("F:/Michelle/hashtag_file.txt", "r") #open text file
+#
+#     hashtag_list = f.readlines()
+#
+#     hashtag_list = list(map(str.strip, hashtag_list)) #get rid of the \r tag from each line
+#
+#     f.close()
+#
+#     return hashtag_list
 
 
 def read_csv():
     with open('F:/Michelle/hashtags.csv', mode='r') as infile:
         reader = csv.reader(infile)
         mydict = {rows[0]: int(rows[1]) for rows in reader}
-        print(mydict)
+        # print(mydict)
         return mydict
 
 
@@ -37,34 +37,39 @@ if __name__ == '__main__':
 
     my_dict = read_csv()
 
-    print(len(my_dict))
+    # print(len(my_dict))
 
     d1 = dict((k, v) for k, v in my_dict.items() if v >= 10000000)
     d2 = dict((k, v) for k, v in my_dict.items() if (v >= 1000000 and v < 10000000))
     d3 = dict((k, v) for k, v in my_dict.items() if (v >= 100000 and v < 1000000))
     d4 = dict((k, v) for k, v in my_dict.items() if v < 100000)
 
-    print(d1)
-    print(len(d1))
-    print(d2)
-    print(len(d2))
-    print(d3)
-    print(len(d3))
-    print(d4)
-    print(len(d4))
+    # print(d1)
+    # print(len(d1))
+    # print(d2)
+    # print(len(d2))
+    # print(d3)
+    # print(len(d3))
+    # print(d4)
+    # print(len(d4))
 
 
 
 
-    candidate_list = d1
+    candidate_list = list(d1.items())
     print(candidate_list)
+    print(len(candidate_list))
     #
-    for count in range(2):
+
+
+    for count in range(1):
+
+        print(count)
 
         length = len(candidate_list) #get length of list
 
         tag = random.randint(0,length-1) #then generate a random integer less than or equal that length
-        print(tag)
+        # print(tag)
 
         post_list.append(candidate_list.pop(tag)) #remove the tag at that position from the list and  put it in a new list
 
