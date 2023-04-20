cc3a3326a54118f6c37e3e6c1745d30b7e2fe7f9
piphomer <phillippeterhomer@gmail.com>
2023-04-20 19:56:12 +0100

    Generate a list of 16 tags, picking a distribution of tags based on their popularity.

--- main.py
+++ main.py
@@ -34,30 +34,31 @@ if __name__ == '__main__':
     # - <100k posts
     # pick 1 from top part, 3 from next part, 6 from next, 6 from last (16 tags total)
 
-    qty_dict = {'d1': 1, 'd2': 3, 'd3': 6, 'd4': 6}
-
     my_dict = read_csv()
-
 
     list1 = list(k for k, v in my_dict.items() if v >= 10000000)
     list2 = list(k for k, v in my_dict.items() if (v >= 1000000 and v < 10000000))
     list3 = list(k for k, v in my_dict.items() if (v >= 100000 and v < 1000000))
     list4 = list(k for k, v in my_dict.items() if v < 100000)
 
-    list_of_lists = [list1, list2, list3,list4]
-
-    # print(list_of_lists)
+    list_of_lists = [list1, list2, list3, list4]
 
     for list in list_of_lists:
 
-        print(list)
+        #Can't find a more elegant way of doing this...
+        if list == list1:
+            qty = 1
+        elif list == list2:
+            qty = 3
+        else: qty = 6
 
-        length = len(list) #get length of list
+        for count in range(qty):
 
-        tag = random.randint(0,length-1) #then generate a random integer less than or equal that length
-        print(tag)
+            length = len(list) #get length of list
 
-        post_list.append(list.pop(tag))  # remove the tag at that position from the list and  put it in a new list
+            tag = random.randint(0,length-1) #then generate a random integer less than or equal that length
+
+            post_list.append(list.pop(tag))  #remove the tag at that position from the list and  put it in a new list
 
     print(post_list)
 
